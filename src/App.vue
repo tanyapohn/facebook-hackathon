7651e5754c10152986e751f9de43ab0704750a1d
Thammarith <thammarith@users.noreply.github.com>
2020-03-07 15:31:01 +0700

    Install vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view /> -->
+  </div>
+</template>
+
+<script>
+import Twitter from "twitter-lite";
+
+const client = new Twitter({
+  consumer_key: "WUDm8KuJMbTfvEWMo4YxDxJkA",
+  consumer_secret: "2JJyKdylCY0bqWv8EFjoSGvKYsDz4wl7EceuSqbVt09bl5CSze"
+});
+
+export default {
+  mounted() {
+    client
+      .getRequestToken("http://localhost:8080/callback")
+      .then(res =>
+        console.log({
+          reqTkn: res.oauth_token,
+          reqTknSecret: res.oauth_token_secret
+        })
+      )
+      .catch(console.error);
+  }
+};
+</script>
